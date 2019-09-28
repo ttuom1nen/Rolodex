1c094b6d04ddc9ba91a773bbbc1f7df8f2d190b0
ttuom1nen <ttuominen@outlook.com>
2019-09-28 22:26:11 +0300

    Card styling

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./card.css";
 
 const Card = props => {
   return (
-    <div>
-      <h1 key={props.monster.id}>{props.monster.name}</h1>
+    <div className="card">
+      <h3 key={props.monster.id}>{props.monster.name}</h3>
     </div>
   );
 };
