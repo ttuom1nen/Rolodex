3820e158676d7c9bed1451438701e455e92e55f5
ttuom1nen <ttuominen@outlook.com>
2019-09-29 22:49:56 +0300

    Filter input

--- src/App.js
+++ src/App.js
@@ -19,6 +19,11 @@ class App extends React.Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
         <input
@@ -26,7 +31,7 @@ class App extends React.Component {
           placeholder="Search robots"
           onChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
