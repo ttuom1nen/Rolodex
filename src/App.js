0abd559cbc34bcd5bfa7372826d7dc3294e06947
ttuom1nen <ttuominen@outlook.com>
2019-09-29 22:44:51 +0300

    Robot images

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -20,6 +21,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="Search robots"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,7 +4,12 @@ import "./card.css";
 const Card = props => {
   return (
     <div className="card">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`}
+      />
       <h3 key={props.monster.id}>{props.monster.name}</h3>
+      <p>{props.monster.email}</p>
     </div>
   );
 };
